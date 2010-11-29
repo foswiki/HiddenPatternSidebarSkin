084811f2d98326a33d0c6404d44deff7f7afaef5
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-11-29 06:31:44 +0000

    Item10090: create a HiddenPatternSidebarSkin (I've had this in a play branch for ages, and just refound it while cherrypicking other things
    git-svn-id: http://svn.foswiki.org/trunk/HiddenPatternSidebarSkin@10095 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Contrib/HiddenPatternSidebarSkin.pm
@@ -0,0 +1,37 @@
+# Skin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+
+package Foswiki::Contrib::HiddenPatternSidebarSkin;
+
+use strict;
+
+# $VERSION is referred to by Foswiki, and is the only global variable that
+# *must* exist in this package. This should always be in the format
+# $Rev: 3193 $ so that Foswiki can determine the checked-in status of the
+# extension.
+our $VERSION = '$Rev$'; # version of *this file*.
+
+# $RELEASE is used in the "Find More Extensions" automation in configure.
+# It is a manually maintained string used to identify functionality steps.
+# You can use any of the following formats:
+# tuple   - a sequence of integers separated by . e.g. 1.2.3. The numbers
+#           usually refer to major.minor.patch release or similar. You can
+#           use as many numbers as you like e.g. '1' or '1.2.3.4.5'.
+# isodate - a date in ISO8601 format e.g. 2009-08-07
+# date    - a date in 1 Jun 2009 format. Three letter English month names only.
+# Note: it's important that this string is exactly the same in the extension
+# topic - if you use %$RELEASE% with BuildContrib this is done automatically.
+our $RELEASE = '1.1.1';
+
+our $SHORTDESCRIPTION = 'all the user to hide and auto-hide the PatternSkin sidebar';
+
